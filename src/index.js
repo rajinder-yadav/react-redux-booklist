25d21ea8583d390a50c6c06fc3c04334eceaa76b
Rajinder Yadav <rajindery@hotmail.com>
2017-09-11 16:22:48 -0400

    Comment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Redux
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
