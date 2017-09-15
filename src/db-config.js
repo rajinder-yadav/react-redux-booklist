308cab0ab5b4fa64e2594159f0fff5876841b67c
Rajinder Yadav <rajindery@hotmail.com>
2017-09-15 16:24:00 -0400

    You need your own firebase config.

--- src/db-config.js
+++ src/db-config.js
@@ -1,8 +1,2 @@
 export const DB_CONFIG = {
-  apiKey: 'AIzaSyApUCljCcVD6YAmjYgymibOZpF9yAgt1pg',
-  authDomain: 'booklist-33cbb.firebaseapp.com',
-  databaseURL: 'https://booklist-33cbb.firebaseio.com',
-  projectId: 'booklist-33cbb',
-  storageBucket: 'booklist-33cbb.appspot.com',
-  messagingSenderId: '285819099522'
 };
