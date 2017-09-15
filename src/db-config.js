ca1c6e51bb6913d825ea0ac0e5ee516ccc33d7cc
Rajinder Yadav <rajindery@hotmail.com>
2017-09-15 07:43:01 -0400

    Single quote consistency.

--- src/db-config.js
+++ src/db-config.js
@@ -1,8 +1,8 @@
 export const DB_CONFIG = {
-  apiKey: "AIzaSyApUCljCcVD6YAmjYgymibOZpF9yAgt1pg",
-  authDomain: "booklist-33cbb.firebaseapp.com",
-  databaseURL: "https://booklist-33cbb.firebaseio.com",
-  projectId: "booklist-33cbb",
-  storageBucket: "booklist-33cbb.appspot.com",
-  messagingSenderId: "285819099522"
+  apiKey: 'AIzaSyApUCljCcVD6YAmjYgymibOZpF9yAgt1pg',
+  authDomain: 'booklist-33cbb.firebaseapp.com',
+  databaseURL: 'https://booklist-33cbb.firebaseio.com',
+  projectId: 'booklist-33cbb',
+  storageBucket: 'booklist-33cbb.appspot.com',
+  messagingSenderId: '285819099522'
 };
--- src/services/BookService.js
+++ src/services/BookService.js
@@ -5,7 +5,7 @@ export class BookService {
     return new Promise((resolve, reject) => {
 
       const database = fire.database().ref().child('books');
-      database.once("value", snapshot => {
+      database.once('value', snapshot => {
         const books = snapshot.val();
         resolve(books);
       });
